a50a9b75fe13bc226ff5058d528fcf78884f2964
CarrotKnits <carrotsandcarrots@gmail.com>
2025-02-06 14:08:29 -0500

    MODIFY props_to_html() method to handle cases of 'props=None' and 'props={}'

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -11,10 +11,13 @@ class HTMLNode:
         raise NotImplementedError("Not Implemented.")
 
     def props_to_html(self):
-        html_attributes_string = ''
-        for prop in self.props:
-            html_attributes_string = html_attributes_string + f' {prop}="{self.props[prop]}"'
-        return html_attributes_string
+        if not self.props: # Handles None or an empty dictionary
+            return ""
+        else:
+            html_attributes_string = ''
+            for prop in self.props:
+                html_attributes_string = html_attributes_string + f' {prop}="{self.props[prop]}"'
+            return html_attributes_string.lstrip()
     
     def __repr__(self):
         return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -4,10 +4,34 @@ from htmlnode import HTMLNode
 
 
 class TestHTMLNode(unittest.TestCase):
-    testing_props = {"href": "https://www.google.com", "target": "_blank",}
 
+    # Main Paths
     def test_props_eq(self):
         node = HTMLNode(None, None, None, {"href": "https://www.google.com", "target": "_blank",})
-        string =  'href="https://www.google.com" target="_blank"'
+        string = 'href="https://www.google.com" target="_blank"'
         print("PtoHTML:",node.props_to_html(), '\nSTRING:',string)
         self.assertEqual(node.props_to_html(), string)
+
+    def test_no_props(self):
+        node = HTMLNode()
+        self.assertEqual(node.props_to_html(), '')
+
+    def test_props_one_prop(self):
+        node = HTMLNode(None, None, None, {"href": "https://www.google.com"})
+        string = 'href="https://www.google.com"'
+        print("PtoHTML:",node.props_to_html(), '\nSTRING:',string)
+        self.assertEqual(node.props_to_html(), string)
+
+
+    # Edge Cases
+    def test_props_empty_dict(self):
+        node = HTMLNode(None, None, None, {})
+        self.assertEqual(node.props_to_html(), '')
+
+    def test_props_space_in_key(self):
+        node = HTMLNode(None, None, None, {"id id": "weird", "target": "_blank",})
+        string = 'id id="weird" target="_blank"'
+        print("PtoHTML:",node.props_to_html(), '\nSTRING:',string)
+        self.assertEqual(node.props_to_html(), string)
+
+    # Failure Cases
