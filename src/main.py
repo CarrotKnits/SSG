f141dfea7203350aa0927f8610de1151d6bd0657
CarrotKnits <carrotsandcarrots@gmail.com>
2026-01-08 13:55:41 -0500

    create main.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,10 @@
-print("Hello World!")
+from textnode import TextNode
+from textnode import TextType
+
+
+def main():
+    new_text_node = TextNode("This is feces", TextType.BOLD, "https://dermalaesthetics.com.au/cdn/shop/articles/poop-emoji.png?v=1724135914")
+    print(new_text_node)
+
+# Start of program
+main()
