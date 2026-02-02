03c06efa0aa2ad8c1946835d89ad030716084050
CarrotKnits <carrotsandcarrots@gmail.com>
2026-02-02 17:24:03 -0500

    create unit tests for function block_to_block_type()

--- src/test_block_functions.py
+++ src/test_block_functions.py
@@ -1,6 +1,6 @@
 import unittest
 
-from block_functions import markdown_to_blocks
+from block_functions import markdown_to_blocks, BlockType, block_to_block_type
 
 
 class TestMarkdownToBlocks(unittest.TestCase):
@@ -72,3 +72,73 @@ This is the same paragraph on a new line
                 "- This is a list\n- with items",
             ],
         )
+
+class TestBlockToBlockType(unittest.TestCase):
+    def test_heading(self):
+        block = "# heading"
+        block_type = block_to_block_type(block)
+        self.assertEqual(BlockType.HEADING, block_type)
+
+    def test_invalid_heading(self):
+        block = "#invalid heading becomes a paragraph"
+        block_type = block_to_block_type(block)
+        self.assertEqual(BlockType.PARAGRAPH, block_type)
+
+    def test_heading_6(self):
+        block = "###### heading"
+        block_type = block_to_block_type(block)
+        self.assertEqual(BlockType.HEADING, block_type)
+
+    def test_code(self):
+        block = """```
+This is a code block
+that is not filled with
+poop or pee.
+        ```"""
+        block_type = block_to_block_type(block)
+        self.assertEqual(BlockType.CODE, block_type)
+
+    def test_quote_with_space(self):
+        block = "> This is a quote"
+        block_type = block_to_block_type(block)
+        self.assertEqual(BlockType.QUOTE, block_type)
+
+    def test_quote_with_no_space(self):
+        block = ">This is quote with no space after the symbol"
+        block_type = block_to_block_type(block)
+        self.assertEqual(BlockType.QUOTE, block_type)
+
+    def test_multiple_quotes(self):
+        block = """>This is a quote with no space after the symbol
+> poop quote
+> last quote"""
+        block_type = block_to_block_type(block)
+        self.assertEqual(BlockType.QUOTE, block_type)
+
+    def test_unordered_list(self):
+        block = """- This is a list
+- in order
+- that is not"""
+        block_type = block_to_block_type(block)
+        self.assertEqual(BlockType.UNORDERED_LIST, block_type)
+
+    def test_ordered_list(self):
+        block = """1. This is a list
+2. that is
+3. in order"""
+        block_type = block_to_block_type(block)
+        self.assertEqual(BlockType.ORDERED_LIST, block_type)
+
+    def test_multiline_paragraph(self):
+        block = """This is a multiline
+paragraph.
+Yes it is."""
+        block_type = block_to_block_type(block)
+        self.assertEqual(BlockType.PARAGRAPH, block_type)
+
+    def test_single_line_paragraph(self):
+        block = "This is a single line paragraph"
+        block_type = block_to_block_type(block)
+        self.assertEqual(BlockType.PARAGRAPH, block_type)
+
+
