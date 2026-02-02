a7eba2ddae2acffc163e20366423482d36bd4737
CarrotKnits <carrotsandcarrots@gmail.com>
2026-02-02 15:29:10 -0500

    create more unit tests

--- src/test_block_functions.py
+++ src/test_block_functions.py
@@ -1,7 +1,7 @@
 import unittest
 
 from block_functions import markdown_to_blocks
-from textnode import TextType, TextNode
+
 
 class TestMarkdownToBlocks(unittest.TestCase):
     def test_multiple_blocks(self):
@@ -24,6 +24,51 @@ This is the same paragraph on a new line
             ],
         )
 
-    #def test_one_block(self):
+    def test_one_block(self):
+        md = """
+- This is a list
+- with items
+"""
+        blocks = markdown_to_blocks(md)
+        self.assertEqual(
+            blocks,
+            [
+                "- This is a list\n- with items",
+            ],
+        )
 
-    #def test_no_blocks(self):
+    def test_no_blocks(self):
+        md = """
+
+
+
+"""
+        blocks = markdown_to_blocks(md)
+        self.assertEqual(blocks,[],)
+
+    def test_repeated_newlines(self):
+        md = """
+This is **bolded** paragraph
+
+
+
+This is another paragraph with _italic_ text and `code` here
+This is the same paragraph on a new line
+
+
+
+
+
+
+- This is a list
+- with items
+"""
+        blocks = markdown_to_blocks(md)
+        self.assertEqual(
+            blocks,
+            [
+                "This is **bolded** paragraph",
+                "This is another paragraph with _italic_ text and `code` here\nThis is the same paragraph on a new line",
+                "- This is a list\n- with items",
+            ],
+        )
