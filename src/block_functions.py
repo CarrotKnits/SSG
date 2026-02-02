897b69aa255cd6f15f21c96e041b2be585ba6a43
CarrotKnits <carrotsandcarrots@gmail.com>
2026-02-02 15:07:48 -0500

    create block_functions.py and function 'markdown_to_blocks()'

---
+++ src/block_functions.py
@@ -0,0 +1,14 @@
+import unittest
+
+from block_functions import markdown_to_blocks
+
+def markdown_to_blocks(markdown): # markdown is a raw Markdown string: represents a full document
+    final_blocks = []
+    striped_blocks = []
+    blocks = markdown.split("\n\n")
+    for block in blocks:
+        striped_blocks.append(block.strip())
+    for block in striped_blocks:
+        if block != "":
+            final_blocks.append(block)
+    return final_blocks
